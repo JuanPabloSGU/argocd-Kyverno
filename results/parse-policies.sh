5fe5d11da61bd720235687dd137a16fc4a875df6
Sanchez Garcia Juan Pablo <juanpablo.sanchezgarcia@dfo-mpo.gc.ca>
2022-10-12 13:36:31 -0400

    parse policies results

---
+++ results/parse-policies.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+parse_data()
+{
+  # Parse Namespace
+  ns=()
+
+  for Namespace in $(kubectl get polr -A | awk 'NR>1{print $1}')
+  do
+    ns+=($Namespace)
+  done
+
+  # Parse Name
+  polr_n=()
+  for Name in $(kubectl get polr -A | awk 'NR>1{print $2}')
+  do
+    polr_n+=($Name)
+  done
+
+  # Put Everything into seperate files
+  
+  for ((i = 0; i < ${#ns[@]}; i++)); do
+    kubectl describe -n ${ns[$i]} polr ${polr_n[$i]} | grep "Result: \+fail" -B10 >> "results/${polr_n[$i]}-result.txt"
+  done
+
+}
+
+parse_data
